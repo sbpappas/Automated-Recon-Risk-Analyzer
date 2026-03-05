dc5f14a012d83d613d1939cf8406fc4e26c548ba
Samuel Pappas <spappas@trinity.edu>
2026-03-04 19:37:15 -0600

    safely check if there is a scan.xml file first

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from scanner import run_scan
 from parser import parse_scan
 from risk_engine import assess_risk
 from report_generator import generate_report
+import os
 
 def main():
     target = input("Enter target IP: ")
 
     run_scan(target) # create file
+    if not os.path.exists("scan.xml"): #be safe
+        print("[!] No scan results found. Exiting.")
+        return
+    
     services = parse_scan("scan.xml") # parse file
     findings = assess_risk(services) #analyze data
     generate_report(findings) #create pretty report
