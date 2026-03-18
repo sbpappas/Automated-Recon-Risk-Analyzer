ec37f5af3044e076710528db72531991026d333d
Samuel Pappas <spappas@trinity.edu>
2026-03-17 21:56:33 -0500

    instructions

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from attack_suggestions import suggest_attacks
 from cve_lookup import lookup_cves  
 import os
 
+# Run with python3 main.py or sudo python3 main.py if you want to run nmap with root privileges
+# then type in your target IP address and wait for report generation (127.0.0.1 is a good test)
+
 def main():
     target = input("Enter target IP: ")
 
